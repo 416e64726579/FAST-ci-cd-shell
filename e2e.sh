60cad9598a8681530807edeebef3025d9e92dc81
Andrey <apetriv@wallarm.com>
2019-09-19 15:15:48 +0300

    Solved WALARM_UUID typo

--- e2e.sh
+++ e2e.sh
@@ -1,7 +1,7 @@
 ####
 echo "Create test run"
 
-TESTRUN=$(./create_test_run.sh $WALARM_UUID $WALARM_SECRET)
+TESTRUN=$(./create_test_run.sh $WALLARM_UUID $WALLARM_SECRET)
 TEST_RECORD_ID=$(echo $TESTRUN | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|grep "\"test_record_id\":" | awk -F ':' '{ print $2 }' | sed -e 's/"//g')
 TEST_ID=$(echo $TESTRUN | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|grep "\"id\":" | awk -F ':' '{ print $3 }' | sed -e 's/"//g')
 
@@ -14,7 +14,7 @@ echo "Test run in paused state created: $TESTRUN"
  sleep 20
 
 echo "Wait until fast is ready"
-(./wait_fast.sh $WALARM_UUID $WALARM_SECRET $TEST_ID)
+(./wait_fast.sh $WALLARM_UUID $WALLARM_SECRET $TEST_ID)
 
 ####
 echo "Run e2e tests (curl commands to app). Recording baselines"
@@ -23,17 +23,17 @@ sleep 10
 
 ####
 echo "Stop recording baselines"
-(./stop_recording.sh $WALARM_UUID $WALARM_SECRET $TEST_RECORD_ID)
+(./stop_recording.sh $WALLARM_UUID $WALLARM_SECRET $TEST_RECORD_ID)
 sleep 10
 
 ####
 echo "Start running security tests"
-(./start_security_test.sh $WALARM_UUID $WALARM_SECRET $TEST_ID)
+(./start_security_test.sh $WALLARM_UUID $WALLARM_SECRET $TEST_ID)
 sleep 10
 
 ####
 echo "Wait untill tests have passed"
-(./wait_test_run.sh $WALARM_UUID $WALARM_SECRET $TEST_ID)
+(./wait_test_run.sh $WALLARM_UUID $WALLARM_SECRET $TEST_ID)
 
 #### Note: No need 'cause the container will be destroyed by gitlab-ci job script
 #echo "Stop fast"
